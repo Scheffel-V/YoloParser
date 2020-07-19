3787e87482fcee7bfd1b220dc09db2d63bf3565c
Scheffel-V <brsufirefox@gmail.com>
2020-07-19 17:19:00 -0300

    Versao final senhoras e senhores

--- YoloParser/src/parser/model/InputObjects.java
+++ YoloParser/src/parser/model/InputObjects.java
@@ -1,23 +1,26 @@
 package parser.model;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class InputObjects {
-	private ArrayList<InputObject> Objects = new ArrayList<InputObject>();
+	private List<InputObject> Objects = new ArrayList<InputObject>();
 	
 	public InputObjects() {
 		
 	}
 	
-	public InputObjects(final ArrayList<InputObject> inputObjects) {
+	public InputObjects(final List<InputObject> inputObjects) {
 		this.setInputObjects(inputObjects);
 	}
 
-	public ArrayList<InputObject> getInputObjects() {
-		return Objects;
-	}
+	public List<InputObject> getInputObjects() {
+        return Objects.stream().filter(java.util.Objects::nonNull).collect(Collectors.toList());
+    }
 
-	public void setInputObjects(ArrayList<InputObject> inputObjects) {
+	public void setInputObjects(List<InputObject> inputObjects) {
 		this.Objects = inputObjects;
 	}
 }
